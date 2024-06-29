80c23b12f4c460b89620ef5fd5915f2244a454c1
benabdallah samir <samirbenabdallah4002@gmail.com>
2024-06-30 01:00:16 +0200

    start navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import Navbar from "./com/Navbar";
 
 function App() {
   return (
-    <main className="w-[100vw] h-[100vh] bg-background-primary font-sans text-text-primary"></main>
+    <main className="w-[100vw] h-[100vh] bg-background-primary font-sans text-text-primary">
+      <Navbar />
+    </main>
   );
 }
 
---
+++ src/com/Navbar.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [showNav, setshowNav] = useState(false);
+  return (
+    <nav className="w-full h-[10vh] bg-background-secondary flex items-center px-2 s2:px-4 relative">
+      <img src="" alt="" className="w-10 h-10 rounded-cercle" />
+      <button
+        className="hidden s1:block ml-auto"
+        onClick={() => setshowNav((curr) => !curr)}
+      >
+        click
+      </button>
+      <ul
+        className={`flex items-center ml-auto capitalize overflow-hidden duration-500 
+                    s1:absolute bottom-0 s1:block s1:translate-y-full s1:w-[300px] border-l-accent-cyan right-0 bg-background-secondary
+                     ${
+                       showNav
+                         ? "s1:h-[90vh] s1:pt-[20vh] s1:border-l "
+                         : "s1:h-0 "
+                     }`}
+      >
+        <li className="ml-10 s1:pl-0 s1:h-10 flex items-center ">
+          <Link
+            to={"/aboutMe"}
+            className="cursor-pointer duration-300 hover:text-highlight-link"
+            onClick={() => setshowNav((curr) => !curr)}
+          >
+            who am i
+          </Link>
+        </li>
+        <li className="ml-10 s1:pl-0 s1:h-10 flex items-center ">
+          <Link
+            to={"/projects"}
+            className="cursor-pointer duration-300 hover:text-highlight-link"
+            onClick={() => setshowNav((curr) => !curr)}
+          >
+            projects
+          </Link>
+        </li>
+        <li className="ml-10 s1:pl-0 s1:h-10 flex items-center ">
+          <Link
+            to={"/nothing"}
+            className="cursor-pointer duration-300 hover:text-highlight-link"
+            onClick={() => setshowNav((curr) => !curr)}
+          >
+            nothing
+          </Link>
+        </li>
+        <li className="ml-10 s1:pl-0 s1:h-10 flex items-center ">
+          <Link
+            to={"/contact"}
+            className="cursor-pointer duration-300 hover:text-highlight-link"
+            onClick={() => setshowNav((curr) => !curr)}
+          >
+            contact me
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
